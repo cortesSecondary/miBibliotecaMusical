5fa6dbd8cc38b9133a776ec7994bea47fa108c14
Jaume Cortés <jaumej07@gmail.com>
2024-02-06 19:31:31 +0100

    HomeFragment and SearchFragment added

--- app/src/main/java/com/example/mibibliotecamusical/MainActivity.kt
+++ app/src/main/java/com/example/mibibliotecamusical/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mibibliotecamusical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mibibliotecamusical.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 }
