7f61290c49d1c2b22b2e73c163521a26bc286b3a
Jaume Cortés <jaumej07@gmail.com>
2024-02-06 18:56:44 +0100

    Librarys added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.mibibliotecamusical"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mibibliotecamusical"
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     viewBinding {
         enable = true
@@ -40,11 +40,40 @@ android {
 }
 
 dependencies {
+    val lifecycleVersion = "2.6.2"
+    val glideVersion = "4.12.0"
+    val coroutinesVersion = "1.6.4"
+    val livedataVersion = "2.4.0"
+    val retrofitVersion = "2.9.0"
+    val lifecycleruntimektxVersion = "2.5.1"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    kapt("com.github.bumptech.glide:compiler:$glideVersion")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$livedataVersion")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+
+    // Gson
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Lifecycle Runtime
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleruntimektxVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
